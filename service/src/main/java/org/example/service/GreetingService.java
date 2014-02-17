1b649f493d14137ed26e7a74e3c556b7cf513bc9
Krzysztof Michalski <krzysiek.michalski@interia.pl>
2014-02-17 18:50:16 +0100

    Added a commented-out example which causes build failure
    Added a commented-out line of code with a TODO-comment in order to show
    what I meant in my StackOverflow question. - Added a reference to the
    Greeter class from the implementaiton module in the GreetingService
    class in the sample-service module.

--- service/src/main/java/org/example/service/GreetingService.java
+++ service/src/main/java/org/example/service/GreetingService.java
@@ -1,5 +1,6 @@
 package org.example.service;
 
+import org.example.core.Greeter;
 import org.example.core.IGreeter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ public class GreetingService
 {
   @Autowired
   IGreeter greeter;
+  
+  // TODO: Uncomment the following line and launch mvn clean install on the whole project in order to see the compilation failure:
+//  private Greeter greeterFromImplementation;
 
   public String greet(final String name)
   {
